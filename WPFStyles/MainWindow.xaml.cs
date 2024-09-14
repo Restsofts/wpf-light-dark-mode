df9d02b373930583992bc5d7e6eb97dd53f99fc0
Restsofts <restsofts@gmail.com>
2024-09-14 14:21:06 +0100

    Add files via upload

---
+++ WPFStyles/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace WPFStyles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool _isDarkMode = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SwitchTheme_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to determine current theme can be added here
+            _isDarkMode = !_isDarkMode /* Determine the current theme */;            
+            ChangeTheme(_isDarkMode ? "Light" : "Dark");
+        }
+
+        private void ChangeTheme(string theme)
+        {
+            var dict = new ResourceDictionary();
+            switch (theme)
+            {
+                case "Dark":
+                    dict.Source = new Uri("DarkTheme.xaml", UriKind.Relative);
+                    break;
+                case "Light":
+                    dict.Source = new Uri("LightTheme.xaml", UriKind.Relative);
+                    break;
+            }
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(dict);
+        }
+    }
+}
